fd803574b5632c0177b171129d80aa4390864f25
Paulo Vilá <paulovila@live.com>
2020-01-12 13:36:43 +0000

    Add demo Uno project with MaterialDesign themes

---
+++ MainDemo.Uno/MainDemo.Uno.Shared/ClickTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.Devices.Input;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace MainDemo.Uwp
+{
+    public class ClickTrigger : StateTriggerBase
+    {
+        public RippleAnimated TargetElement
+        {
+            get { return (RippleAnimated)GetValue(TargetElementProperty); }
+            set { SetValue(TargetElementProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetElementProperty =
+            DependencyProperty.Register("TargetElement", typeof(RippleAnimated), typeof(ClickTrigger),
+                new PropertyMetadata(null, (d, e) => (d as ClickTrigger).AttachElement(e.NewValue as RippleAnimated)));
+
+        RippleAnimated _targetElement;
+        private void AttachElement(RippleAnimated fe)
+        {
+            if (_targetElement != null)
+                _targetElement.PressedPointEvent = null;
+            _targetElement = fe;
+            _targetElement.PressedPointEvent = OnTapped;
+        }
+        private async void OnTapped()
+        {
+            SetActive(true);
+            await Task.Delay(300);
+            SetActive(false);
+        }
+    }
+    public class RippleAnimated : ContentControl, INotifyPropertyChanged
+    {
+        protected override void OnPointerPressed(PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerDeviceType == PointerDeviceType.Mouse)
+            {
+                var p = e.GetCurrentPoint(this);
+                CurrentPosition = new Point { X = p.Position.X - ActualWidth / 3, Y = p.Position.Y - ActualHeight / 3 };
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentPosition"));
+                PressedPointEvent?.Invoke();
+            }
+            base.OnPointerPressed(e);
+        }
+        public Point CurrentPosition { get; set; }
+        public Action PressedPointEvent;
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ MainDemo.Uno/MainDemo.Uno.Shared/MyUserControl1.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MainDemo.Uno
+{
+    public sealed partial class MyUserControl1
+    {
+        public MyUserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
